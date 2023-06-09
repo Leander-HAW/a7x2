e141bc975381630b5aff14fdfcae6f0b1bd45a44
Leander-HAW <leander.hofherr@haw-hamburg.de>
2023-06-09 23:14:07 +0200

    Test
    Test

--- ItemProcessor/ItemProcessor.java
+++ ItemProcessor/ItemProcessor.java
@@ -1,5 +1,38 @@
 package ItemProcessor;
 
-public class ItemProcessor {
+
+
+import java.util.*;
+
+import thingy.Item;
+
+public class ItemProcessor implements ItemProcessor_I {
+	private Map<Item, List<Item>> fiveDifferent;
+	private Set<Set<Item>> sevensome;
+
+	public ItemProcessor() {
+		this.fiveDifferent = new HashMap<Item, List<Item>>();
+		this.sevensome = new HashSet<Set<Item>>();
+	}
+	
+	
+	@Override
+	public Collection<Item> process(Item item) {
+		
+		Set<Item> sevensome = new HashSet<Item>();
+		return null;
+	}
+
+	@Override
+	public void reset() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
--- ItemProcessor/ItemProcessor_I.java
+++ ItemProcessor/ItemProcessor_I.java
@@ -1,5 +1,19 @@
 package ItemProcessor;
 
+import thingy.*;
+import java.util.Collection;
+
 public interface ItemProcessor_I {
+
+    final int requestedNumberOfThingsOfSameSize = 7;
+    
+    Collection<Item> process( Item item );
+    
+    
+    void reset();
+
+    boolean isEmpty();
+
+
 
 }
